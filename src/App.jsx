fe2eef3ffa411a053a73ddb36728df52c5a0b99c
Sufiyan <csesufi@gmail.com>
2025-09-26 18:54:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "../Pages/Home/Home";
-import Products from "../Pages/Product/Products";
-import ProductDetail from "../Pages/ProductDetails/ProductDetail";
-import Signup from "../Pages/Signup/Signup";
-import Login from "../Pages/Login/Login";
-import Cart from "../Pages/Cart/Cart";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Product/Products";
+import ProductDetail from "./Pages/ProductDetails/ProductDetail";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Cart from "./Pages/Cart/Cart";
+import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
@@ -20,7 +22,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
