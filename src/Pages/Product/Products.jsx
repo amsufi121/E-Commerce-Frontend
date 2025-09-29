475ea533822ddf3e22d75596dda4f4550e93c800
Attull <atul852798@gmail.com>
2025-09-29 19:44:50 +0530

    filter feature

--- src/Pages/Product/Products.jsx
+++ src/Pages/Product/Products.jsx
@@ -5,12 +5,16 @@ import ProductDetail from "../ProductDetails/ProductDetail";
 
 const Products = () => {
   const [productData, setProductData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [srch, setSrch] = useState("");
+  const [dValue, setDvalue] = useState("");
 
   function fetchProducts() {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => {
         setProductData(data);
+        setFilterData(data);
         console.log(data);
       })
       .catch((error) => console.error("Error", error));
@@ -22,37 +26,63 @@ const Products = () => {
 
   const navigate = useNavigate();
 
+  function filterProducts(e) {
+    setSrch(e);
+
+    const fData = productData.filter((prod) => {
+      return prod.title.toUpperCase().includes(srch.toUpperCase());
+    });
+
+    setFilterData(fData);
+  }
+
+  useEffect(() => {
+    const fData = productData.filter((prod) => {
+      return prod.category == dValue;
+    });
+
+    setFilterData(fData);
+  }, [dValue]);
+
   return (
-    <div className="products-container">
-      {productData.map((product) => (
-        <div key={product.id} className="product-card">
-          <div className="product-img">
-            <img
-              src={product.image}
-              onClick={() => navigate("/ProductDetail")}
-            />
+    <>
+      <input onChange={(e) => filterProducts(e.target.value)} />
+      <select onChange={(e) => setDvalue(e.target.value)}>
+        <option value="electronics">Electronics </option>
+        <option value="jewelery">Jewelery </option>
+        <option value="men's clothing">Men's clothing </option>
+      </select>
+      <div className="products-container">
+        {filterData.map((product) => (
+          <div key={product.id} className="product-card">
+            <div className="product-img">
+              <img
+                src={product.image}
+                onClick={() => navigate("/ProductDetail")}
+              />
+            </div>
+
+            <h4 className="product-title">{product.title}</h4>
+
+            <p className="product-description">{product.description}</p>
+
+            <div className="product-details">
+              <span className="product-price">${product.price}</span>
+
+              <span className="product-rating">
+                {" "}
+                ⭐ {product.rating.rate} ({product.rating.count})
+              </span>
+              <span className="product-category">{product.category}</span>
+            </div>
+
+            <div></div>
+
+            <button className="cart-btn">Add to Cart</button>
           </div>
-
-          <h4 className="product-title">{product.title}</h4>
-
-          <p className="product-description">{product.description}</p>
-
-          <div className="product-details">
-            <span className="product-price">${product.price}</span>
-
-            <span className="product-rating">
-              {" "}
-              ⭐ {product.rating.rate} ({product.rating.count})
-            </span>
-            <span className="product-category">{product.category}</span>
-          </div>
-
-          <div></div>
-
-          <button className="cart-btn">Add to Cart</button>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
